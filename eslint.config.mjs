a1c7fef9257ee961137657561b0b1b0ea951223d
narr07 <dinarpermadi07@gmail.com>
2025-03-01 12:54:21 +0700

    fix: update @nuxt/ui version to 3.0.0-beta.2 and remove eslint-plugin-tailwindcss from configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
 // @ts-nocheck
-
 import antfu from '@antfu/eslint-config'
-// import pluginTailwindCSS from 'eslint-plugin-tailwindcss'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
@@ -15,9 +13,5 @@ export default withNuxt(
     vue: true,
     stylistic: true,
     typescript: true,
-  }),
-  // {
-  //   plugins: { tailwindcss: pluginTailwindCSS },
-  //   rules: pluginTailwindCSS.configs.recommended.rules,
-  // },
+  })
 )
