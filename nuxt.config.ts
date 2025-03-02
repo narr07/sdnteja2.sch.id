b9e38fd5b44c865118ae546cebca6542e20e7c19
narr07 <dinarpermadi07@gmail.com>
2025-03-02 12:23:44 +0700

    refactor: clean up template formatting and enhance nuxt.config.ts with additional configuration options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({ 
- 
-  modules: ['@nuxt/ui', '@nuxt/content', '@nuxthub/core'],
+  modules: ['@nuxt/ui',   '@nuxt/content',  '@nuxthub/core', ],
   css: ['~/assets/css/main.css'],
- 
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+  },
+  hub: {
+    database: true,
+  }, 
+  content: {
+    database: {
+      type: 'd1',
+      bindingName: 'teja2',
+    },
+  },
+
+  icon: {
+    customCollections: [
+      {
+        prefix: 'narr',
+        dir: './app/assets/icons',
+      },
+    ],
+  },
   nitro: {
     prerender: {
       routes: ['/'],
