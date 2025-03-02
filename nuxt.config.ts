13954db6a89f65f1a92d80cde667088d8c1796d9
narr07 <dinarpermadi07@gmail.com>
2025-03-02 12:04:55 +0700

    feat: add @nuxthub/core dependency and update nuxt.config.ts to include it in modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({ 
  
-  modules: ['@nuxt/ui', '@nuxt/content'],
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxthub/core'],
   css: ['~/assets/css/main.css'],
  
   nitro: {
