135718683d3a5b3b5243a58d4abafc84e43b6259
narr07 <dinarpermadi07@gmail.com>
2025-03-04 03:51:54 +0700

    fix: update @nuxt/content dependency version; correct image source reference in Guru list component

--- app/components/Guru/List.server.vue
+++ app/components/Guru/List.server.vue
@@ -15,7 +15,7 @@ const { data: guruTeja } = await useAsyncData('gurus', () => {
         <div v-for="guru in guruTeja" :key="guru.nama" class="transition-transform duration-300 ease-in-out transform hover:scale-98 ">
           <NuxtLink :to="guru.meta.path as string" class="p-4">
             <UCard data-aos="fade-up" variant="soft" class="bg-night-50 shadow-teja dark:bg-night-900 h-full  rounded-4xl">
-              <NuxtImg :src="guru.foto.src" alt="Guru Photo" width="234" height="234" class="rounded-lg mb-4" />
+              <NuxtImg :src="guru.foto" alt="Guru Photo" width="234" height="234" class="rounded-lg mb-4" />
               <h3 class="text-lg font-bold">
                 {{ guru.nama }}
               </h3>
