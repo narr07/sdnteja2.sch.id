0f7d8c7b46dbf573effa7683e3ec79c2d98e8308
narr07 <dinarpermadi07@gmail.com>
2025-03-04 04:04:31 +0700

    refactor: update content configuration to use named export for collections; streamline guru collection schema

--- content.config.ts
+++ content.config.ts
@@ -1,30 +1,30 @@
-import { defineCollection, defineContentConfig, z } from '@nuxt/content'
+import { defineCollection, z } from '@nuxt/content'
 
-export default defineContentConfig({
-  collections: {
-    content: defineCollection({
-      type: 'page',
-      source: '**',
-    }),
-    guru: defineCollection({
-      type: 'data',
-      source: 'guru/**.yml',
-      schema: z.object({
-        nama: z.string(),
-        kelas: z.string(),
-        foto: z.object({
-          src: z.string().editor({ input: 'media' }),
-          alt: z.string().default(function (this: { nama: string }) { return this.nama }), // alt diambil dari nama
-        }),
-        catatan: z.string(),
-        nip: z.string(),
-        jabatan: z.string(),
-        pendidikan: z.string(),
-        pengalaman: z.array(z.object({
-          title: z.string(),
-          tahun: z.string(),
-        })),
+export const collections = ({
+
+  content: defineCollection({
+    type: 'page',
+    source: '**',
+  }),
+  guru: defineCollection({
+    type: 'data',
+    source: 'guru/**.yml',
+    schema: z.object({
+      nama: z.string(),
+      kelas: z.string(),
+      foto: z.object({
+        src: z.string().editor({ input: 'media' }),
+        alt: z.string().default(function (this: { nama: string }) { return this.nama }), // alt diambil dari nama
       }),
+      catatan: z.string(),
+      nip: z.string(),
+      jabatan: z.string(),
+      pendidikan: z.string(),
+      pengalaman: z.array(z.object({
+        title: z.string(),
+        tahun: z.string(),
+      })),
     }),
-  },
+  }),
+
 })
