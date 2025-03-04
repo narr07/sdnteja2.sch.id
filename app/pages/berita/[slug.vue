3cfd9df91dd7dc9d81e9f16827a20ccd1cb5f7b7
narr07 <dinarpermadi07@gmail.com>
2025-03-05 00:25:45 +0700

    refactor: update content configuration for guru entries; rename fields for localization and add berita page component

---
+++ app/pages/berita/[slug.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const route = useRoute()
+
+const { data: beritaPage } = await useAsyncData(`berita-${route.path}`, () => {
+  return queryCollection('guru').path(route.path).first()
+})
+</script>
+
+<template>
+  <div>
+    <UContainer>
+      <div class="py-8">
+        <h1 data-aos="fade-up" class="text-5xl text-center md:text-6xl font-bold">
+          {{ beritaPage?.title }}
+        </h1>
+      </div>
+      <div class="prose dark:prose-dark">
+        <ContentRenderer v-if="beritaPage" :value="beritaPage" />
+      </div>
+    </UContainer>
+  </div>
+</template>
